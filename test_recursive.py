ea9fef88475c93d04530f8cad7f1eb6af7d7311a
j0sephmerheb <joseph@merheb.net>
2020-04-28 14:07:36 +0200

    Working console Version

--- test_recursive.py
+++ test_recursive.py
@@ -1,19 +1,39 @@
-grid = [ 
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 1, 1, 1, 0, 1, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1, 0, 1, 2, 0, 1],
-    [1, 0, 1, 0, 0, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-]
+grid = []
 
+
+# Step 1
+# Read matrix from text file, and save into the grid[]
+filepath = "maze1.txt"
+def read_file(filepath):    
+    with open(filepath) as my_txt:        
+        # For each line
+        for line in my_txt: 
+            # Remove line break and spaces
+            line = line.replace("\n","").split(" ")
+
+            # Add new list to the main grid
+            grid.append(line)            
+    #print(grid)
+
+    # Run function. Starting from 1,1
+    deep_index(grid, "s")
+
+
+# Step 2
+# Search for index of element in sub-list.
+# It will return (x,y)
+def deep_index(lst, w):
+    for (i, sub) in enumerate(lst):
+        if w in sub:
+            search(i, sub.index(w))
+    
+
+
+# Step 3
+# Read generated grid and search for path
 def search(x, y):
     # Reached the end
-    if grid[x][y] == 2:
+    if grid[x][y] == 'e':
         print('found at %d,%d' % (x, y))
         return True
     # Reached a wall
@@ -24,6 +44,7 @@ def search(x, y):
     elif grid[x][y] == 3:
         print('visited at %d,%d' % (x, y))
         return False
+
     
     # Visiting square
     print('visiting %d,%d' % (x, y))
@@ -40,5 +61,19 @@ def search(x, y):
 
     return False
 
-# Run function. Starting from 1,1
-search(1, 1)
+
+
+        
+
+
+#################### STEPS ########################
+# Step 1: Read file and generate grid
+read_file(filepath)
+
+
+# Step 2: Search for 's'
+# callback of step 2
+
+
+# Step 3: Run through the grid
+# callback of step 3
